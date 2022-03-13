fd51437a7c791c4d4978d38e86cdf0af5a8285d6
Joey Sutcliffe <joeysutcliffe0@gmail.comgit config --global user.email joeysutcliffe0@gmail.com>
2022-03-13 20:32:54 +0100

    update

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
+// --------------------------------------------------round 2
 require("dotenv").config();
-
+const axios = require("axios");
 const express = require("express");
 const hbs = require("hbs");
-
-const app = express();
 const PORT = 3000;
+
+// require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 
-const SpotifyWebApi = require("spotify-web-api-node");
-
-// require spotify-web-api-node package here:
+// setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 });
-
-// console.log(spotifyApi);
 
 // Retrieve an access token
 spotifyApi
@@ -30,30 +32,50 @@ spotifyApi
 
 // Our routes go here:
 
-app.get("/artist-search", (req, res) => {
-  //   const { artist } = req.data.artist;
-
+app.get("/artist-search-results", async (req, res) => {
   spotifyApi
-    .searchArtists(req.query.artistName)
+    .searchArtists(artist)
     .then((data) => {
-      console.log("The received data from the API: ", data.body);
-      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-
-      return res.render("artist-search-results", {
-        artist: data.body.artist.items,
+      const apiResponse = data.get(SpotifyWebApi, {
+        params: { name: req.query.artist },
       });
 
-      console.log(artist);
+      console.log(apiResponse);
+      console.log(req.body);
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
+
+  // res.render("artist-search-results");
+  res.render("artist-search-results", {
+    name: apiResponse.data,
+    // imgUrl: apiResponse.image,
+  });
+
+  // try {
+  //   const apiResponse = await axios.get(SpotifyWebApi, {
+  //     params: { name: req.query.aritist },
+  //   });
+  //   console.log(apiResponse.data);
+  //   const [foundArtist] = data.body.artist;
+  //   //character is the name of the key that we put on the html page
+  //   // res.render("character", { character: req.query.character });
+  //   res.render("artist-search-results", {
+  //     name: foundArtist.name,
+  //     // imgUrl: firstCharacterFound.image,
+  //   });
+  // } catch (err) {
+  //   return res.send(`Sorry mate, i cant find that artist in the system 😅`);
+  // }
 });
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(3000, () =>
+app.listen(PORT, () =>
   console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
 );
+
+// --------------------------------------------------round 2
